a2e8fb1c4c079f9040c4c4fd90085669c240be1b
Koushik <koushik.nittur@gmail.com>
2024-02-18 19:50:21 +0530

    Add login method with completion

--- SingletonDemo/SingletonDemo/Singleton.swift
+++ SingletonDemo/SingletonDemo/Singleton.swift
@@ -5,12 +5,26 @@
 //  Created by Koushik A N on 18/02/24.
 //
 
-import Foundation
+import UIKit
+
+// Singleton
+
+struct LoggedInUser {}
 
 final class APIClient {
     static let instance = APIClient()
     
     private init() {}
+    
+    func login(completion: (LoggenInUser) -> Void) {}
+}
+
+class LoginViewController: UIViewController {
+    func didTapLoginButton() {
+        APIClient.instance.login() { user in
+            // show next screen
+        }
+    }
 }
 
 let apiClient = APIClient.instance
