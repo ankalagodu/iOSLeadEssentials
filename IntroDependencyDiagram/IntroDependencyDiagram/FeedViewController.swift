9c100ceebbdd01246bb9198fd1d7246169f98d1a
Koushik <koushik.nittur@gmail.com>
2024-02-24 21:51:50 +0530

    Add local feed loader when failed to load remote

--- IntroDependencyDiagram/IntroDependencyDiagram/FeedViewController.swift
+++ IntroDependencyDiagram/IntroDependencyDiagram/FeedViewController.swift
@@ -11,24 +11,43 @@ protocol FeedLoader {
     func loadFeed(completion: @escaping ([String]) -> Void)
 }
 
+struct Reachable {
+    static var isNetworkAvailable = false
+}
+
 class FeedViewController: UIViewController {
-    var loader: FeedLoader!
+    var remote: FeedLoader!
+    var local: FeedLoader!
     
-    convenience init(loader: FeedLoader) {
+    convenience init(remote: FeedLoader, local: FeedLoader) {
         self.init()
-        self.loader = loader
+        self.local = local
+        self.remote = remote
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loader.loadFeed { items in
-            // Update UI
+        if Reachable.isNetworkAvailable {
+            remote.loadFeed { loadItems in
+                // Update UI
+            }
+        } else {
+            local.loadFeed { loadItems in
+                // Update UI
+            }
         }
+        
     }
 }
 
 class RemoteFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
+
+class LocalFeedLoader: FeedLoader {
     func loadFeed(completion: @escaping ([String]) -> Void) {
         // do something
     }
