dd9986bb2acd70b32a730918588e346ca032a1b0
Koushik <koushik.nittur@gmail.com>
2024-03-17 11:05:37 +0530

    Create Feed item model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Koushik A N on 09/03/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
