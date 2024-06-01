d8ae15d23dc730eb22e55c4e52e4c46b8a2a6fc9
Koushik <koushik.nittur@gmail.com>
2024-06-01 20:30:41 +0530

    Add `MamagedCache` and `ManagedFeedImage` core data entity class implementation and helper function for `NSManageObjectContext` extension

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,31 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Koushik A N on 01/06/24.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+final class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+extension ManagedCache {
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name ?? "")
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+}
