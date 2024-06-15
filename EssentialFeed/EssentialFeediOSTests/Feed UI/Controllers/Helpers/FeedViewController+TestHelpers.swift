f0cb1cde153b7f2f4b0b187d36619adf03d9b4a0
Koushik <koushik.nittur@gmail.com>
2024-06-15 20:07:59 +0530

    Move `UIButton` helper method to TestHelpers file

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewController+TestHelpers.swift
@@ -61,13 +61,3 @@ extension FeedViewController {
         0
     }
 }
-
-extension UIButton {
-    func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
