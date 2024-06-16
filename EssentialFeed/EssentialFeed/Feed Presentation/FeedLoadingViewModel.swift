74be9b828c7ef74f519bd1db584aa3e16ee52c7e
Koushik <koushik.nittur@gmail.com>
2024-06-16 18:53:04 +0530

    Move `FeedViewModel`, `FeedLoadingViewModel` and `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Koushik A N on 16/06/24.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
